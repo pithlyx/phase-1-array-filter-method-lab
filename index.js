9c7d78c45b167424a2f8c744e16541aa177a7a64
Cody Roberts <pithlyx@gmail.com>
2023-05-02 16:15:07 -0600

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array, string){
+    let filteredArr = array.filter((element) => element.toLowerCase() == string.toLowerCase())
+    //console.log(filteredArr)
+    return filteredArr
+}
+
+function fuzzyMatch(array, string){
+    let fuzzyArr = array.filter((element) => element.toLowerCase().slice(0, string.length) === string.toLowerCase())
+    return fuzzyArr
+}
+
+function matchName(array, string){
+    let matchedArr = array.filter((element) => element['name'] === string)
+    console.log(matchedArr)
+    return matchedArr
+}
